c822aaa4e31d111b710678d98ef020041a5d420f
IrakliMeto <ika.metonidze@gmail.com>
2020-06-18 15:39:40 +0400

    add function factorial

--- algorithm.js
+++ algorithm.js
@@ -30,3 +30,15 @@ function convertFToC(farenheit) {
 console.log(convertFToC(55));
 
 //////////////////////////
+
+function numberFactorial(num) {
+  let sum = 1;
+  for (let i = 2; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
+}
+
+console.log(numberFactorial(13));
+
+////////////////////////
